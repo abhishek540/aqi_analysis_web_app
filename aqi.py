d69308334485dcc60a70eb6b0038d1386ab3b79c
abhishek540 <41794440+abhishek540@users.noreply.github.com>
2022-05-06 08:03:08 +0530

    Update aqi.py

--- aqi.py
+++ aqi.py
@@ -7,7 +7,7 @@ This is a temporary script file.
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,accuracy_score
 from sklearn.ensemble import ExtraTreesRegressor
